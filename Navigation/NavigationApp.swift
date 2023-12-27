2be998caa01b2f14305911c95de19de4918d7a34
Daniel Yankiver <dyankiver@gmail.com>
2023-12-27 10:27:34 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Daniel Yankiver on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
